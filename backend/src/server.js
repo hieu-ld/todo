fbe63b720f0a227ef66669f4603cadba19a32493
hieu-ld <duchieu.6931@gmail.com>
2026-03-11 11:45:45 +0700

    fix route

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ if (process.NODE_ENV !== "production") {
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get(".*", (req, res) => res.sendFile(path.join(__dirname, "../frontend/dist/index.html")));
+    app.get("/{*path}", (req, res) => res.sendFile(path.join(__dirname, "../frontend/dist/index.html")));
 }
 
 // sử dụng routes
